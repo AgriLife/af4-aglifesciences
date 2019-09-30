1e28f8266682a7f451e36ce156ff8b5f1dcf80db
Zachary Watkins <watkinza@gmail.com>
2019-09-30 09:46:58 -0500

    Fix LiveWhale event tag closure

--- templates/home.php
+++ templates/home.php
@@ -156,7 +156,7 @@ function home_content() {
 		}
 
 		$l_event_list .= sprintf(
-			'<div class="event cell auto"><div class="grid-x grid-padding-x"><div class="cell date shrink"><div class="month h3">%s</div><div class="h2 day">%s</div></div><div class="cell title auto"><a href="%s" title="%s" class="event-title truncate truncate-2">%s</a><div class="location truncate truncate-2">%s</div></div></div></div></div>',
+			'<div class="event cell auto"><div class="grid-x grid-padding-x"><div class="cell date shrink"><div class="month h3">%s</div><div class="h2 day">%s</div></div><div class="cell title auto"><a href="%s" title="%s" class="event-title truncate truncate-2">%s</a><div class="location truncate truncate-2">%s</div></div></div></div>',
 			$date_month,
 			$date_day,
 			$url,
