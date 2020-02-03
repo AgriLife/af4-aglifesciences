ee114bbad4d3ca92792e926a3649bbe80c21d482
Zachary Watkins <watkinza@gmail.com>
2020-02-03 11:14:25 -0600

    Add header logos from af4-college plugin

--- src/class-genesis.php
+++ src/class-genesis.php
@@ -26,6 +26,36 @@ class Genesis {
 	 */
 	public function __construct() {
 
+		add_filter( 'af4_header_logo', array( $this, 'header_logo' ), 11, 4 );
+
+	}
+
+	/**
+	 * Header logo and title
+	 *
+	 * @since 0.8.5
+	 * @param string $inside Current title inner HTML.
+	 * @param string $old_inside Previous title inner HTML.
+	 * @param string $logo_html HTML template string.
+	 * @param string $home Homepage url.
+	 * @return string
+	 */
+	public function header_logo( $inside, $old_inside, $logo_html, $home ) {
+
+		$inside = sprintf(
+			'<div class="logo"><a href="%s" title="%s"><img class="logo-long" src="%s" alt="%s"><img class="logo-long-light" src="%s" alt="%s"><img class="logo-break" src="%s" alt="%s"></a></div>',
+			$home,
+			get_bloginfo( 'name' ),
+			ALSAF4_DIR_URL . 'images/logo-coals-long.svg',
+			get_bloginfo( 'name' ),
+			ALSAF4_DIR_URL . 'images/logo-coals-long-white.svg',
+			get_bloginfo( 'name' ),
+			ALSAF4_DIR_URL . 'images/logo-coals-break-white.svg',
+			get_bloginfo( 'name' )
+		);
+
+		return $inside;
+
 	}
 
 }
